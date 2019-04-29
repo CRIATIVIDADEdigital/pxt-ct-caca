d1c6f6821e5588f446133b9ddb2c1d530b2fe0e8
Admin CRIATIVIDADE.digital <32722216+criatividade-digital@users.noreply.github.com>
2019-04-29 17:59:33 -0300

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+let signal = 0;
+radio.onReceivedNumber(function (receivedNumber) {
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    if (signal < -90) {
+        basic.showIcon(IconNames.SmallDiamond)
+    } else if (signal < -80) {
+        basic.showIcon(IconNames.Diamond)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
+})
+radio.setGroup(1)
